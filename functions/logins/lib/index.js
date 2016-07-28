ef620ef961974d0e79293bf349ee84ecb34bb2b6
RyanDubowsky <r.k.dubowsky@gmail.com>
2016-07-28 11:15:26 -0400

    Changed SQL statement to return a row for every date in the interval, even if there is no logins that day

--- functions/logins/lib/index.js
+++ functions/logins/lib/index.js
@@ -12,9 +12,18 @@ module.exports.respond = function(event,context,cb){
 
   var client = new Client(config);
   client.connect();
-  var query = client.query("SELECT COUNT(*), date_part('day', timestamp) as day, date_part('month', timestamp) as month FROM login_stats WHERE timestamp > current_date - interval '30' day GROUP BY day,month ORDER BY month, day", function(err, result) {
-
-
+  var query = client.query(
+    "SELECT dt.series, COUNT(timestamp)\
+    FROM login_stats \
+    RIGHT OUTER JOIN (\
+      SELECT \
+      GENERATE_SERIES( (NOW() - INTERVAL '30 day')::date, NOW()::date, '1 day')::date\
+      AS series\
+    ) AS dt \
+    on date_trunc('day', timestamp) = dt.series\
+    GROUP BY dt.series\
+    ORDER BY dt.series ASC", 
+    function(err, result) {
     cb(null,result.rows);
 
     client.end(function (err) {
